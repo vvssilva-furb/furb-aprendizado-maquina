edc178915d479dbd7906b2e3ee81937e26f82250
Eduardo Reinert <eduardo.a.reinertt@gmail.com>
2025-04-09 19:43:21 -0300

    Início, trabalho 2

---
+++ src/trab2_knn/implementacao_knn.py
@@ -0,0 +1,10 @@
+import scipy.io as scipy
+
+def meuKnn(dadosTrain, rotuloTrain, dadosTeste, k):
+    print("tst")
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
